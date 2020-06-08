25c4630ac493c2b39ff6d6f7370d16add9b8f542
Maxim Sigalov <xamvolagis@gmail.com>
2020-06-08 19:03:39 -0400

    大丈夫

--- run.py
+++ run.py
@@ -32,20 +32,21 @@ def run_day(status):
   # TODO: at least weight this by the expectation or something.
 
   for row in data.itertuples():
-    if status.cash < MIN_POSITION_SIZE:
+    if status.alpaca.buying_power < MIN_POSITION_SIZE:
       print('Ran out of money for opening positions')
     qty = TARGET_POSITION_SIZE // row.limit
     try:
-      alpaca.submit_order(symbol=row.symbol,
-                          qty=qty,
-                          side='buy',
-                          type='limit',
-                          order_class="bracket",
-                          time_in_force="gtc",
-                          limit_price=row.limit,
-                          take_profit={'limit_price': row.take_profit},
-                          stop_loss={'stop_price': row.stop,
-                                     'limit_price': row.stop_limit})
+      status.alpaca.submit_order(
+          symbol=row.symbol,
+          qty=qty,
+          side='buy',
+          type='limit',
+          order_class="bracket",
+          time_in_force="gtc",
+          limit_price=row.limit,
+          take_profit={'limit_price': row.take_profit},
+          stop_loss={'stop_price': row.stop,
+                     'limit_price': row.stop_limit})
       print("Order for {} for {}$ at {}$, profit at {}, stop/limit at {}/{} completed.".format(row.symbol,
                                                                                                qty*row.limit,
                                                                                                row.take_profit,
@@ -62,7 +63,7 @@ def run_day(status):
     print('Ran out of positions to open')
 
   # Print summaries every hour.
-  while alpaca.get_clock_is_open():
+  while status.alpaca.get_clock_is_open():
     util.summarize()
     time.sleep(3600)
 
